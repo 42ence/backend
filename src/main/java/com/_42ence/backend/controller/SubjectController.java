772da59f2c2ab0b33dc0394aa29d365f101051ec
임원정 <wonlim@student.42seoul.kr>
2023-02-02 00:16:37 +0900

    feat : add wiki  post api #11
    공부 목록 post api

--- src/main/java/com/_42ence/backend/controller/SubjectController.java
+++ src/main/java/com/_42ence/backend/controller/SubjectController.java
@@ -1,11 +1,14 @@
 package com._42ence.backend.controller;
 
+import com._42ence.backend.dto.Request.WikiRequestDTO;
 import com._42ence.backend.dto.Response.SubjectListResponseDTO;
 import com._42ence.backend.dto.Response.SubjectResponseDTO;
 import com._42ence.backend.dto.Response.WikiResponseDTO;
 import com._42ence.backend.service.SubjectService;
 import com._42ence.backend.service.WikiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,5 +35,19 @@ public class SubjectController {
     @GetMapping("/wiki/{subjectName}")
     public WikiResponseDTO getSubjectWiki(@PathVariable String subjectName) {
         return wikiService.getSubjectWiki(subjectName);
+    }
+
+    @PostMapping("/wiki")
+    public ResponseEntity<Void> saveWikiInfo(@RequestBody WikiRequestDTO wikiRequestDTO){
+        // wiki 버전이 같은 지 확인한다.
+        boolean check = wikiService.checkWikiVersion(wikiRequestDTO);
+
+        if (!check){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        // 버전이 똑같은 걸 확인하고 저장한다.
+        wikiService.write(wikiRequestDTO);
+        // 저장한 버전을
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/_42ence/backend/service/WikiService.java
+++ src/main/java/com/_42ence/backend/service/WikiService.java
@@ -1,5 +1,6 @@
 package com._42ence.backend.service;
 
+import com._42ence.backend.dto.Request.WikiRequestDTO;
 import com._42ence.backend.dto.Response.WikiResponseDTO;
 import com._42ence.backend.entity.Subject;
 import com._42ence.backend.entity.Wiki;
@@ -33,5 +34,44 @@ public class WikiService {
         wikiResponseDTO.setEditor(wiki.getEditor());
 
         return wikiResponseDTO;
+    }
+
+    public void write(WikiRequestDTO wikiRequestDTO){
+        // 저장을 하는데, 저장할 때 버전을 이전에서 +1 하고 저장
+        Wiki wiki = convertWikiRequestDToToEntity(wikiRequestDTO);
+        wikiRepository.save(wiki);
+
+        // 저장하고 그 wiki id를 서브젝트 테이블에 저장
+        wiki = wikiRepository.findById(wikiRepository.getMaxId()).get();
+        Subject subject = subjectRepository.findByName(wikiRequestDTO.getSubjectName());
+        subject.setWikiVersion(wiki.getVersion());
+        subjectRepository.save(subject);
+    }
+
+    private Wiki convertWikiRequestDToToEntity(WikiRequestDTO wikiRequestDTO) {
+        Subject subject = subjectRepository.findByName(wikiRequestDTO.getSubjectName());
+        Wiki wiki = new Wiki();
+        wiki.setSubject(subject);
+        wiki.setBody(wikiRequestDTO.getBody());
+        wiki.setEditor(wikiRequestDTO.getIntraId());
+        wiki.setVersion(wikiRequestDTO.getVersion() + 1);
+
+        return wiki;
+    }
+
+    /***
+     * 버전이 같은 지 비교하는 함수
+     */
+    public boolean checkWikiVersion(WikiRequestDTO wikiRequestDTO) {
+
+        Subject subject = subjectRepository.findByName(wikiRequestDTO.getSubjectName());
+
+        if (wikiRequestDTO.getVersion() == 0)
+            return true;
+
+        if (subject.getWikiVersion() == wikiRequestDTO.getVersion())
+            return true;
+        else
+            return false;
     }
 }
